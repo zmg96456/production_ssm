3f854b99f839ebad6e87b386123fb4d865447b3e
zmg96456 <964568945@qq.com>
2020-05-21 15:23:18 +0800

    机构增删改查

---
+++ src/main/java/com/megagao/production/ssm/domain/Branch.java
@@ -0,0 +1,26 @@
+package com.megagao.production.ssm.domain;
+
+public class Branch {
+	private String id;
+	private String name;
+	private String short_name;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getShort_name() {
+		return short_name;
+	}
+	public void setShort_name(String short_name) {
+		this.short_name = short_name;
+	}
+	
+}
